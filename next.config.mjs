d59aee3e69606a22dd4aa1ae9fa4b7c369aabefd
kashish.bhandula <kashish.bhandula@spyne.ai>
2024-06-29 14:35:18 +0530

    setup pre-commit and module federation

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
+import { NextFederationPlugin } from "@module-federation/nextjs-mf";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, options) => {
+    const { isServer } = options;
+    config.experiments = { topLevelAwait: true };
+    config.devtool = "source-map"; // Corrected the placement of this line
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "hostApp",
+        remotes: {
+          remoteApp: `remoteApp@http://localhost:3001/_next/static/${isServer ? "ssr" : "chunks"}/remoteEntry.js`,
+        },
+        filename: "static/chunks/remoteEntry.js",
+      }),
+    );
+    return config;
+  },
+};
 
 export default nextConfig;
